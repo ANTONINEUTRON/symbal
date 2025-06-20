439e0a689c9aa9db027b71cb7bbcbecf6226ef70
bobo <antobuilds@gmail.com>
2025-06-20 09:44:35 +0100

    now running

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+
+const config = {
+  ...getDefaultConfig(__dirname),
+};
+
+module.exports = wrapWithReanimatedMetroConfig(config);
